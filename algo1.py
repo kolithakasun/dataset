f41ffd7e78d19f6d73a72fa88488e045a0d65307
Madhuvantha K.A.N <37581397+Nawod@users.noreply.github.com>
2021-10-30 15:10:10 +0530

    Create algo1.py

---
+++ algo1.py
@@ -0,0 +1,14 @@
+#Python program to add three numbers
+#with the help of above designed algorithm
+
+
+#Take the 3 number as inputs
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+num3 = int(input("Enter the 3rd number: "))
+
+#Calculate the sum using + operator and store it in variable sum
+sum = num1 + num2 + num3
+
+#Print the sum
+print("\nSum of the 3 number is: ", sum)
